fa3a07104e657153d0b7372608c70b996f7040e3
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2024-02-27 09:03:02 -0300

    fix implements interface

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,5 +1,21 @@
+class LocalSavePurchases {
+    constructor (private readonly cacheStore: CacheStore) {
+
+    }
+}
+
+interface CacheStore{
+
+}
+
+class CacheStoreSpy implements CacheStore {
+    deleteCallsCount = 0
+}
+
 describe('LocalSavePurchases', () => {
-    test('', () => {
-        expect(1).toBe(1)
+    test('Should not delete cache on sut.init', () => {
+        const cacheStore = new CacheStoreSpy()
+        new LocalSavePurchases(cacheStore)
+        expect(cacheStore.deleteCallsCount).toBe(0)
     })
 })
