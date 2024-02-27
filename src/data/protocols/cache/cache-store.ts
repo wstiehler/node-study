2c3651d3f413c399b5e044eff141f60a4136b58f
William Villani Stiehler <william.stiehler@evoluinfo.com.br>
2024-02-27 09:29:47 -0300

    refactor code

---
+++ src/data/protocols/cache/cache-store.ts
@@ -0,0 +1,4 @@
+
+export interface CacheStore {
+    delete: (key: string) => void
+}
---
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -0,0 +1,39 @@
+import { CacheStore } from "../../../data/protocols/cache/index"
+import { LocalSavePurchases } from "./local-save-purchases"
+
+
+class CacheStoreSpy implements CacheStore {
+    deleteCallsCount = 0
+    key: string
+
+    delete(key: string): void {
+        this.deleteCallsCount++
+        this.key = key
+    }
+}
+
+interface SutTypes {
+    sut: LocalSavePurchases
+    cacheStore: CacheStoreSpy
+}
+
+const makeSut = (): SutTypes => {
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalSavePurchases(cacheStore)
+    return {
+        sut, cacheStore
+    }
+}
+
+describe('LocalSavePurchases', () => {
+    test('Should not delete cache on sut.init', () => {
+        const {cacheStore} = makeSut()
+        expect(cacheStore.deleteCallsCount).toBe(0)
+    })
+    test('Should delete old cache on sut.save', async () => {
+        const {cacheStore, sut} = makeSut()
+        await sut.save()
+        expect(cacheStore.deleteCallsCount).toBe(1)
+        expect(cacheStore.key).toBe('purchases')
+    })
+})
